08a60f23c93a39f25a6172c4c9c245f0e33fdbd5
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2023-08-28 07:34:31 -0400

    Navbar changes mobile view
    some esthetic view

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 // import React from 'react'
 import { NavbarBackGround } from '../../vectors/NavbarBackGround'
-import './Navbar.css'
+import Styles from './Navbar.module.css'
 import { Link } from 'react-router-dom'
 import {HOME_URL, SKILLS_URL, ABOUT_URL, EXPERIENCE_URL, CONTACT_URL} from '../../constants/urls.js'
 import {mePicture} from '../../constants/images.js'
+import { useWindowWidth } from '../../hooks/useWindowWidth'
+import { NavbarMobile } from '../../vectors/navBarMobile'
 
 export default function Navbar() {
 
@@ -26,30 +28,46 @@ export default function Navbar() {
     },
   ]
 
+  const {windowWidth} = useWindowWidth()
+
   return (
-    <div className='NavbarContainer'>
-      <NavbarBackGround className={'NavbarBackGround'}/>
+    <>
+      {windowWidth > 500 ? (
+        <>
+          <div className={Styles.NavbarContainer}>
+            <NavbarBackGround className={Styles.NavbarBackGround}/>
 
-      <header className="navbarOptions">
-        <ul className="leftOptions">
+            <header className={Styles.navbarOptions}>
+              <ul className={Styles.leftOptions}>
 
-          {
-            leftSideOptions.map((option, index) => (
-              <li key={index}>
-                <Link to={option.href} className='Link'>
-                  <span>{option.text}</span>
-                </Link>
-              </li>
-            ))
-          }
+                {
+                  leftSideOptions.map((option, index) => (
+                    <li key={index}>
+                      <Link to={option.href} className={Styles.Link}>
+                        <span>{option.text}</span>
+                      </Link>
+                    </li>
+                  ))
+                }
 
-        </ul>
+              </ul>
 
-        <Link to={HOME_URL} className='rigthOptions'>
-            <img src={mePicture}/>
-        </Link>
-      </header>
+              <Link to={HOME_URL} className={Styles.rigthOptions}>
+                  <img src={mePicture}/>
+              </Link>
+            </header>
 
-    </div>
+          </div>
+        </>
+      ) : (
+        <div className={Styles.navbarMobile}>
+          <button>
+            <NavbarMobile />
+
+          </button>
+        </div>
+      ) 
+      }
+    </>
   )
 }
