8f5c11e461787372407efaeb5883d9c50bdd084c
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2023-09-06 10:34:40 -0400

    added text-align center on Carrousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react'
 import {Motion, spring} from 'react-motion';
 import Styles from './Carousel.module.css';
-import { pictures } from '../../constants/images';
-
+import { images } from '../../constants/images';
+import { Link } from 'react-router-dom';
 
 export function Carousel() {
     // const images = pictures;
@@ -11,7 +11,7 @@ export function Carousel() {
 
         useEffect(() => {
           const intervalId = setInterval(() => {
-            const length = pictures.length - 1;
+            const length = images.length - 1;
             
             if (selectedIndex == length) {
               setSelectedIndex(0);
@@ -27,9 +27,10 @@ export function Carousel() {
   return (
     <div className={Styles.imagesContainer}>
 
-    {pictures.map((image, index) => (
+    {images.map((image, index) => (
+      // <Link key={index} to={image.href} target='_blank' className={Styles.linkImageSelected}>
       <Motion
-        key={image}
+        key={index}
         defaultStyle={{ translateX: selectedIndex === index ? 0 : 110 }}
         style={{ translateX: spring(selectedIndex === index ? 0 : (index + 1) === selectedIndex ? -103 : (index - 1) === selectedIndex ? 103   : index < selectedIndex ? -150 : 150)}}
       >
@@ -38,14 +39,17 @@ export function Carousel() {
             className={selectedIndex === index ? Styles.imageSelected : Styles.imageNotSelected}
             style={{ transform: `translateX(${style.translateX}%)`}}
           
-          >
+            >
           <img
-            src={image}
+            src={image.image}
             />
-          <div className='gradientGreyLayer'/>
+          <div className='gradientGreyLayer'>
+            <h1>{image.definition}</h1>
+          </div>
           </div>
             )}
       </Motion>
+      // </Link>
     ))}
   </div>
   )
--- src/constants/images.js
+++ src/constants/images.js
@@ -8,16 +8,55 @@ import rm2 from '../images/rm2.png'
 import resumeHomeOnPhone from '../images/resumeHomeOnPhone.png'
 import carteleraCaracas from '../images/carteleraCaracas.png'
 import carteleraCaracas2 from '../images/carteleraCaracas2.png'
+import onibusClientProfile from '../images/onibusClientProfile.jpg'
 
-export const pictures = [
-    meOnAvila,
-    // me,
-    workingOnNavbar,
-    rm1,
-    rm2,
-    resumeHomeOnPhone,
-    carteleraCaracas,
-    carteleraCaracas2,
+export const images = [
+    {
+        image: meOnAvila,
+        definition: '',
+        href: 'https://www.zeiss.es/vision-care/mejor-vision/salud-y-prevencion/cual-es-la-mejor-manera-de-limpiar-y-cuidar-sus-gafas.html#:~:text=Despu%C3%A9s%20aclare%20las%20gafas%20en,dejar%C3%A1%20marcas%20en%20los%20cristales.',
+    },
+    {
+        image: workingOnNavbar,
+        definition: 'Vite + React.js',
+        href: 'https://www.zeiss.es/vision-care/mejor-vision/salud-y-prevencion/cual-es-la-mejor-manera-de-limpiar-y-cuidar-sus-gafas.html#:~:text=Despu%C3%A9s%20aclare%20las%20gafas%20en,dejar%C3%A1%20marcas%20en%20los%20cristales.',
+    },
+    {
+        image: rm1,
+        definition: 'Vite + React.js',
+        href: 'https://www.zeiss.es/vision-care/mejor-vision/salud-y-prevencion/cual-es-la-mejor-manera-de-limpiar-y-cuidar-sus-gafas.html#:~:text=Despu%C3%A9s%20aclare%20las%20gafas%20en,dejar%C3%A1%20marcas%20en%20los%20cristales.',
+    },
+    {
+        image: rm2,
+        definition: 'Vite + React.js',
+        href: 'https://www.zeiss.es/vision-care/mejor-vision/salud-y-prevencion/cual-es-la-mejor-manera-de-limpiar-y-cuidar-sus-gafas.html#:~:text=Despu%C3%A9s%20aclare%20las%20gafas%20en,dejar%C3%A1%20marcas%20en%20los%20cristales.',
+    },
+    {
+        image: resumeHomeOnPhone,
+        definition: 'Vite + React.js (view on phone)',
+        href: 'https://www.zeiss.es/vision-care/mejor-vision/salud-y-prevencion/cual-es-la-mejor-manera-de-limpiar-y-cuidar-sus-gafas.html#:~:text=Despu%C3%A9s%20aclare%20las%20gafas%20en,dejar%C3%A1%20marcas%20en%20los%20cristales.',
+    },
+    {
+        image: carteleraCaracas,
+        definition: 'Vite + React.js',
+        href: 'https://www.zeiss.es/vision-care/mejor-vision/salud-y-prevencion/cual-es-la-mejor-manera-de-limpiar-y-cuidar-sus-gafas.html#:~:text=Despu%C3%A9s%20aclare%20las%20gafas%20en,dejar%C3%A1%20marcas%20en%20los%20cristales.',
+    },
+    {
+        image: carteleraCaracas2,
+        definition: 'Vite + React.js',
+        href: 'https://www.zeiss.es/vision-care/mejor-vision/salud-y-prevencion/cual-es-la-mejor-manera-de-limpiar-y-cuidar-sus-gafas.html#:~:text=Despu%C3%A9s%20aclare%20las%20gafas%20en,dejar%C3%A1%20marcas%20en%20los%20cristales.',
+    },
+    {
+        image: onibusClientProfile,
+        definition: 'React Native + Typescript',
+        href: 'https://www.zeiss.es/vision-care/mejor-vision/salud-y-prevencion/cual-es-la-mejor-manera-de-limpiar-y-cuidar-sus-gafas.html#:~:text=Despu%C3%A9s%20aclare%20las%20gafas%20en,dejar%C3%A1%20marcas%20en%20los%20cristales.',
+    },
+    
+    
+    
+    
+    
+    
 ]
 
 // export const mePicture = me;
