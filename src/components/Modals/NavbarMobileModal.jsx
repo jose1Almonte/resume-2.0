0c0d0bcdca18ff18876d701bb898e49ac846c0ea
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2023-09-22 17:07:35 -0400

    navbar mobile

--- src/components/Modals/NavbarMobileModal.jsx
+++ src/components/Modals/NavbarMobileModal.jsx
@@ -1,9 +1,13 @@
 // import React from 'react'
 import { Link } from 'react-router-dom'
 import { ABOUT_URL, CONTACT_URL, EXPERIENCE_URL, HOME_URL, SKILLS_URL } from '../../constants/urls'
-import Styles from './NavbarMobileModal.module.css'
+import Styles from './NavbarMobileModal.module.css';
+import { GiCancel } from "react-icons/gi";
+import { useWindowWidth } from '../../hooks/useWindowWidth';
 
 export default function NavbarMobileModal({showNavbar, setShowNavbar, scrollToSection}) {
+  const {windowWidth} = useWindowWidth();
+  
   const leftSideOptions = [
     {
       href: 'home',
@@ -29,18 +33,25 @@ export default function NavbarMobileModal({showNavbar, setShowNavbar, scrollToSe
       <div className={` ${Styles.optionsBox} ${showNavbar ? Styles.appear : Styles.disappear}`}>
           
         <>
+        <div className={Styles.closeNavbarContainer}>
         <button className = {Styles.closeNavbarButton} onClick={() => {setShowNavbar(!showNavbar)}}>
-          <span className = {Styles.closeNavbarText}>Close Navbar</span>
+          {/* <span className = {Styles.closeNavbarText}> */}
+            <GiCancel size={windowWidth/9} color='rgba(255,255,255,0.5)'/>
+          {/* </span> */}
         </button>
+        </div>
+
+        <div className={Styles.optionsContainer}>
         {
-            leftSideOptions.map((option, index) => (
-              <li key={index} className={Styles.option}>
-                <button className={Styles.Link} onClick={() => {scrollToSection(option.href); setShowNavbar(!showNavbar)}}>
-                  <span className={Styles.spanMobileNavbarOption}>{option.text}</span>
+          leftSideOptions.map((option, index) => (
+                <button key={index} className={Styles.Link} onClick={() => {scrollToSection(option.href); setShowNavbar(!showNavbar)}}>
+                  <li className={Styles.option}>
+                      <span className={Styles.spanMobileNavbarOption}>{option.text}</span>
+                  </li>
                 </button>
-              </li>
             ))
           }
+        </div>
         </>
         
       </div>
